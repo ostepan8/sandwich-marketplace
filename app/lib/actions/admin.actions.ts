35a2ca0e78a828fe55745fe797a1d93e29645074
Owen Stepan <ohstep23@gmail.com>
2024-03-26 15:58:16 -0700

    user actions

---
+++ app/lib/actions/admin.actions.ts
@@ -0,0 +1,44 @@
+"use server";
+import jwt from 'jsonwebtoken';
+
+
+const handleError = (error: unknown) => {
+    if (error instanceof Error) {
+      // This is a native JavaScript error (e.g., TypeError, RangeError)
+      console.error(error.message);
+      throw new Error(`Error: ${error.message}`);
+    } else if (typeof error === "string") {
+      // This is a string error message
+      console.error(error);
+      throw new Error(`Error: ${error}`);
+    } else {
+      // This is an unknown type of error
+      console.error(error);
+      throw new Error(`Unknown error: ${JSON.stringify(error)}`);
+    }
+  };
+type LoginProps = {
+    name:string,
+    password:string
+}
+
+
+export async function adminLogin({ name, password }:LoginProps) {
+    const JWT_SECRET = process.env.JWT_SECRET || '';
+    try {
+      if (name === process.env.ADMIN_USERNAME && password === process.env.ADMIN_PASSWORD) {
+        // Credentials are correct, create a JWT token
+        const token = jwt.sign(
+          { username: name },
+          JWT_SECRET, // Ensure you have a secret for JWT generation stored in environment variables
+          { expiresIn: '1h' } // Token expires in 1 hour, adjust based on needs
+        );
+        
+        return { status: 'success', token }; // Send the token to the client
+      } else {
+        throw new Error('Invalid credentials'); // Throw error for wrong credentials
+      }
+    } catch (error) {
+      throw error; // Handle or throw error
+    }
+  }
--- components/customOrderCard.tsx
+++ components/customOrderCard.tsx
@@ -1,12 +1,81 @@
-import React from "react";
-import { Card, CardHeader, CardBody, CardFooter, Avatar, Button } from "@nextui-org/react";
-import { MenuItemProps } from "@/constants/types";
+"use client"
+import React, { useEffect, useState } from "react";
+import { Card, CardHeader, CardBody, CardFooter, Avatar, Button, Select, SelectItem, CheckboxGroup, Checkbox } from "@nextui-org/react";
+import { Ingredient, MenuItem, MenuItemProps } from "@/constants/types";
 import { subtitle, title, } from "./primitives";
-
+import { INGREDIENT_TYPES } from "@/constants";
+import { getMenuAndIngredientData } from "@/app/lib/actions/menu.actions";
+type MenuAndIngredientData = {
+    ingredientData: Ingredient[],
+    menuData: MenuItem[]
+}
 export default function CustomOrderCard() {
-
+    const [breadItems, setBreadItems] = useState<Ingredient[]>([]);
+    const [cheeseItems, setCheeseItems] = useState<Ingredient[]>([]);
+    const [meatItems, setMeatItems] = useState<Ingredient[]>([]);
+    const [toppingItems, setToppingItems] = useState<Ingredient[]>([]);
+    const [sauceItems, setSauceItems] = useState<Ingredient[]>([]);
     const name = "Customize Sandwich"
-    const description = "Choose any meat, cheese, toppings, and condiments!"
+    const [newSandwichData, setNewSandwichData] = useState({
+        name: '',
+        description: '',
+        basePrice: 0,
+        available: true,
+        _id: '',
+        ingredients: []
+    })
+    const [newSandwichIngredients, setNewSandwichIngredients] = useState<Ingredient[]>([]);
+    const handleIngredientChange = (selectedIngredient: Ingredient) => {
+        setNewSandwichIngredients((currentIngredients) => {
+            // Check if the ingredient is already selected
+            const isAlreadySelected = currentIngredients.some(ingredient => ingredient.name === selectedIngredient.name);
+            if (isAlreadySelected) {
+                // Remove the ingredient if it's already selected
+                return currentIngredients.filter(ingredient => ingredient.name !== selectedIngredient.name);
+            } else {
+                // Add the ingredient if it's not already selected
+                return [...currentIngredients, selectedIngredient];
+            }
+        });
+    };
+    async function getIngredientData() {
+        const { menuData, ingredientData }: MenuAndIngredientData = await getMenuAndIngredientData();
+        const tempBreadItems: Ingredient[] = [];
+        const tempCheeseItems: Ingredient[] = [];
+        const tempMeatItems: Ingredient[] = [];
+        const tempToppingItems: Ingredient[] = [];
+        const tempSauceItems: Ingredient[] = [];
+        ingredientData.forEach((ingredient) => {
+            switch (ingredient.type) {
+                case "Bread":
+                    tempBreadItems.push(ingredient);
+                    break;
+                case "Cheese":
+                    tempCheeseItems.push(ingredient);
+                    break;
+                case "Meat":
+                    tempMeatItems.push(ingredient);
+                    break;
+                case "Topping":
+                    tempToppingItems.push(ingredient);
+                    break;
+                case "Sauce":
+                    tempSauceItems.push(ingredient);
+                    break;
+
+            }
+        });
+        setBreadItems(tempBreadItems);
+        setCheeseItems(tempCheeseItems);
+        setMeatItems(tempMeatItems);
+        setToppingItems(tempToppingItems);
+        setSauceItems(tempSauceItems);
+    }
+
+    useEffect(() => {
+        getIngredientData()
+    }, []);
+
     return (
         <Card className="w-[100%]">
             <CardHeader className="justify-between">
@@ -22,16 +91,40 @@ export default function CustomOrderCard() {
             </CardHeader>
             <CardBody className="px-3 py-0 text-small text-default-400">
 
-                {description && (<> <div className="flex flex-col items-start justify-center">
-                    <h1 className={title()}>Description</h1>
+                <div className="flex flex-col md:flex-row md:flex-wrap md:justify-between h-full">
+                    {INGREDIENT_TYPES.map((type: String) => (
+                        (type === "Bread") ?
+                            <div key={type.valueOf()} className="flex-1 min-w-[280px] max-w-xs md:max-w-none py-4 px-2 md:px-4">
+                                <Select
+                                    onChange={(e) => console.log(e.target.value)}
+                                    variant="underlined"
+                                    size="lg"
+                                    label="Select bread type"
+                                >
+                                    {(type === "Bread" ? breadItems : type === "Cheese" ? cheeseItems : type === "Meal" ? meatItems : type === "Topping" ? toppingItems : sauceItems).map((ingredient: Ingredient) => (
+                                        <SelectItem key={ingredient.name.toString()} value={ingredient.name.toString()}>
+                                            {ingredient.name}
+                                        </SelectItem>
+                                    ))}
+                                </Select>
+                            </div>
+                            :
+                            <div className="flex-1 min-w-[280px] py-4 px-2 md:px-4" key={type.valueOf()}>
+                                <CheckboxGroup size="lg" label={`Select ${type}(s)`}>
+                                    {(type === "Bread" ? breadItems : type === "Cheese" ? cheeseItems : type === "Meat" ? meatItems : type === "Topping" ? toppingItems : sauceItems).map((ingredient) => (
+                                        <Checkbox
+                                            key={ingredient.name.toString()}
+                                            value={ingredient.name.toString()}
+                                            onChange={() => handleIngredientChange(ingredient)}
+                                            checked={newSandwichIngredients.some(item => item.name === ingredient.name)}
+                                        >
+                                            {ingredient.name}
+                                        </Checkbox>
+                                    ))}
+                                </CheckboxGroup>
+                            </div>
+                    ))}
                 </div>
-                    <p>
-                        {description}
-                    </p></>)}
-                {/* <div className="flex flex-col items-start justify-center">
-                    <h4 className={title()}>Ingredients</h4>
-                </div>
-                <h2 className={subtitle()}>{ingredientString}</h2> */}
 
             </CardBody>
             <CardFooter className="gap-3 w-full justify-between">
