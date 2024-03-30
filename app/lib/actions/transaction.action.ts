9399473f04ddf66b0a80b38e5c886d41557beb7b
Owen Stepan <ohstep23@gmail.com>
2024-03-29 19:31:26 -0700

    final product

--- app/lib/actions/transaction.action.ts
+++ app/lib/actions/transaction.action.ts
@@ -119,18 +119,65 @@ export async function getAllData(): Promise<GetAllDataToReturn> {
           },
         },
       },
-      {
-        $project: {
-          cartItemsPopulated: 0,
-        },
-      },
     ]);
+    const uncompletedItems = transactions.filter((item) => !item.completed);
+    const completedItems = transactions.filter((item) => item.completed);
+    const convertTimeToMinutes = (timeString: string) => {
+      if (timeString === "Now") {
+        return -1; // Special value for "Now" to prioritize it in sorting
+      }
+
+      const [time, modifier] = timeString.split(" ");
+      let [hours, minutes] = time.split(":").map(Number);
+
+      // Adjust for PM times not being "12", to convert to 24-hour format
+      if (modifier === "PM" && hours !== 12) {
+        hours += 12;
+      } else if (modifier === "AM" && hours === 12) {
+        hours = 0; // Adjust for midnight being represented as "12:00 AM"
+      }
+
+      // Calculate total minutes from 0:00 to handle sorting accurately
+      return hours * 60 + minutes;
+    };
+
+    const sortedItems = uncompletedItems.sort((a, b) => {
+      const minutesA = convertTimeToMinutes(a.pickUpTime);
+      const minutesB = convertTimeToMinutes(b.pickUpTime);
+      const createdAtA = new Date(a.createdAt).getTime();
+      const createdAtB = new Date(b.createdAt).getTime();
+
+      // Handle both "Now" to sort by createdAt
+      if (minutesA === -1 && minutesB === -1) {
+        return createdAtA - createdAtB;
+      }
+      // Prioritize "Now" for item A
+      else if (minutesA === -1) {
+        return -1;
+      }
+      // Prioritize "Now" for item B (corrected to return 1)
+      else if (minutesB === -1) {
+        return 1;
+      }
+
+      // Primary comparison by pickUpTime when not "Now"
+      if (minutesA !== minutesB) {
+        return minutesA - minutesB;
+      }
+
+      // Secondary comparison by createdAt when pickUpTime is equal
+      return createdAtA - createdAtB;
+    });
 
     const menuData: MenuItem[] = await MenuItemDatabase.find()
       .populate("ingredients")
       .lean();
     const ingredientData: Ingredient[] = await IngredientDatabase.find().lean();
-    return { menuData, ingredientData, transactions };
+    return {
+      menuData,
+      ingredientData,
+      transactions: [...sortedItems, ...completedItems],
+    };
   } catch (error) {
     handleError(error);
     return { menuData: [], ingredientData: [], transactions: [] };
